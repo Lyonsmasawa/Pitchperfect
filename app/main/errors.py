0acf7470e6930b905c21f6e62534c40d1cb223ff
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-05 13:09:24 +0300

    start over

--- app/main/errors.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-from . import main #import the blueprint instance main
-
-@main.app_errorhandler(404) #application wide error handler
-def four_Ow_four(error):
-    return render_template('fourOwfour.html'),404
