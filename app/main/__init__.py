6504fcc9afc4d731b0d09ba651fed21c067fc824
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-05 10:23:57 +0300

    define blueprint

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
 main = Blueprint('main', __name__) #name of the blueprint and __name__ variable to find the location of the blueprint
-from . import views, errors
+from . import views, errors #we import to avoid circular dependencies
