bc6226fe602b0274e245fa02442c96f9a1107c6c
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-06 11:54:16 +0300

    create test manager

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+from imghdr import tests
 from re import S
 from app import create_app
 from flask_script import Manager, Server
@@ -9,6 +10,12 @@ app = create_app('development')
 manager = Manager(app)
 #this command launches our server
 manager.add_command('server', Server) 
+
+@manager.command #create new command
+def test():
+    import unittest
+    tests = unittest.TestLoader.discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
