5090f9273703165bda5888abd3901dfe2148761d
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-06 11:23:14 +0300

    set up configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
+from distutils.command.config import config
 from flask import Flask 
+from config import config_options
 
-def create_app(config_options):
+def create_app(config_name):
 
+    #create app instance
     app = Flask(__name__)
+
+    #configuration
+    app.config.from_object(config_options[config_name])
+
 
     return app
