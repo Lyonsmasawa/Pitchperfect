fe29dbd424bc4418139c8299a9e3772f2c6bc235
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-06 21:39:14 +0300

    add User and Test class

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -8,5 +9,18 @@ class User(db.Model):
     email = db.Column(db.String(255), unique = True, index = True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String(255))
+    pass_hash = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('not allowed')
+
+    @password.setter
+    def password(self, password):
+        self.pass_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_hash, password)
+
     
 
