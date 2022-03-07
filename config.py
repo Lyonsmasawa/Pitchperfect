c5f84c22ee1e63b26fe0aa3fcd12c9b9a5312244
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-07 08:59:51 +0300

    add authentication

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:passdb@localhost/pitches'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST = 'app/static/img'
+    MAIL_SERVER = 'smtp.googlemail.com' #smtp server
+    MAIL_PORT = 587 #gmail smtp server port
+    MAIL_USE_TLS = True #enables a transport layer security to secure emails when sending
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME") #these are yours
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde  configurations im
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class ProductionConfig(Config):
     pass
