9afe68e2426489f1c74c29d056b706bd2b07d32d
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-07 21:59:04 +0300

    create models

--- app/models.py
+++ app/models.py
@@ -1,34 +1,23 @@
+from email.policy import default
 from enum import unique
 from importlib_metadata import email
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
-from datetime import datetime
+from datetime import date, datetime
 
-class Movie:
-    
-    def __init__(self, id, title, overview, poster, vote_average, vote_count):
-        self.id =id
-        self.title = title
-        self.overview = overview
-        self.poster = 'https://image.tmdb.org/t/p/w500/'+ poster
-        self.vote_average = vote_average
-        self.vote_count = vote_count
+class Pitch(db.Model):
 
-class Review(db.Model):
-
-    __tablename__ = 'reviews'
+    __tablename__ = 'pitches'
 
     id = db.Column(db.Integer,primary_key = True)
-    movie_id = db.Column(db.Integer)
-    movie_title = db.Column(db.String)
-    image_path = db.Column(db.String)
-    movie_review = db.Column(db.String)
-    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    title = db.Column(db.Integer)
+    content = db.Column(db.String)
     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id')) #tels alchemy foreign key and is the primary key of roles
 
-    def save_review(self):
+    def save_pitch(self):
         db.session.add(self)
         db.session.commit()
 
@@ -45,8 +34,7 @@ class User(UserMixin, db.Model): #arg helps connect  to db
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id')) #tels alchemy foreign key and is the primary key of roles
-    reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
+    pitch = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
 
     @property #create write only class property password
     def password(self):
@@ -61,6 +49,64 @@ class User(UserMixin, db.Model): #arg helps connect  to db
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer,primary_key = True)
+    category = db.Column(db.String(255), unique=True)
+    pitch = db.Column('Pitch', backref = 'category', lazy='dynamic')
+
+    def save_category(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'User {self.category}'
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.Integer)
+    posted = db.Column(db.DateTime, index = True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id')) #tels alchemy foreign key and is the primary key of roles
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'User {self.comment}'
+
+class UpVote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer,primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id')) #tels alchemy foreign key and is the primary key of roles
+
+    def save_vote(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'User {self.id}'
+
+class UpVote(db.Model):
+    __tablename__ = 'downvotes'
+
+    id = db.Column(db.Integer,primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id')) #tels alchemy foreign key and is the primary key of roles
+
+    def save_vote(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'User {self.id}'
 
 
 @login_manager.user_loader
